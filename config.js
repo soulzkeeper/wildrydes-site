deb1bae4a8311aff8792fd8053b6f9cb08f4b7f4
soulzkeeper <34148605+soulzkeeper@users.noreply.github.com>
2021-11-19 22:31:26 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_YBeXRmAIb',
+        userPoolClientId: '4rs0h83mlet06k4sk6qru8v5cm',
+        region: 'us-east-2'
+    },
+    api: {
+        invokeUrl: 'https://k2cdp5ols3.execute-api.eu-west-1.amazonaws.com/prod'
+    }
+};
